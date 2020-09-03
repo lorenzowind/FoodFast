8c598acaebf662bb8ed4691400897357bb604275
lorenzowind <lorenzomart01@gmail.com>
2020-09-03 13:42:45 -0400

    feat: initial, register and login screens with animation

--- mobile/app/src/main/java/com/example/foodfast/MainActivity.java
+++ mobile/app/src/main/java/com/example/foodfast/MainActivity.java
@@ -3,12 +3,80 @@ package com.example.foodfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_signin, button_signup;
+    LinearLayout default_buttons, popup_signup, popup_signin;
+    ImageView button_close_signup, button_close_signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_signin = findViewById(R.id.button_signin);
+        button_signup = findViewById(R.id.button_signup);
+
+        default_buttons = findViewById(R.id.default_buttons);
+        popup_signup = findViewById(R.id.popup_signup);
+        popup_signin = findViewById(R.id.popup_signin);
+
+        button_close_signup = findViewById(R.id.button_close_signup);
+        button_close_signin = findViewById(R.id.button_close_signin);
+
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottomtotop);
+                Animation animation_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+                popup_signup.startAnimation(animation_in);
+                default_buttons.startAnimation(animation_out);
+                popup_signup.setVisibility(View.VISIBLE);
+                default_buttons.setVisibility(View.GONE);
+            }
+        });
+
+        button_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottomtotop);
+                Animation animation_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+                popup_signin.startAnimation(animation_in);
+                default_buttons.startAnimation(animation_out);
+                popup_signin.setVisibility(View.VISIBLE);
+                default_buttons.setVisibility(View.GONE);
+            }
+        });
+
+        button_close_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.toptobottom);
+                Animation animation_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+                popup_signup.startAnimation(animation_out);
+                default_buttons.startAnimation(animation_in);
+                popup_signup.setVisibility(View.GONE);
+                default_buttons.setVisibility(View.VISIBLE);
+            }
+        });
+
+        button_close_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.toptobottom);
+                Animation animation_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+                popup_signin.startAnimation(animation_out);
+                default_buttons.startAnimation(animation_in);
+                popup_signin.setVisibility(View.GONE);
+                default_buttons.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
